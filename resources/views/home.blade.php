bd5e376cd76493a8cb5b4840f72aea0823507d7b
Alessandro Caiti <acaiti@hotmail.it>
2020-06-16 15:31:54 +0200

    x

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        {{-- <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
 
@@ -17,7 +17,7 @@
                     You are logged in!
                 </div>
             </div>
-        </div>
+        </div> --}}
     </div>
 </div>
 @endsection
