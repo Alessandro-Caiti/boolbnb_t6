6dedaa67da19dae82d5def51919183e7823d451f
tarq-p <pierluigi.tarquinio90@gmail.com>
2020-06-17 15:47:55 +0200

    Grafica 8 (bozza di footer)

--- resources/views/user/places/show.blade.php
+++ resources/views/user/places/show.blade.php
@@ -64,9 +64,42 @@
         </div>
     </main>
     <footer>
-      <div class="logo-img">
-        <img src="https://clipart.info/images/ccovers/1499955328airbnb-2-logo-png.png" alt="logo">
-      </div>
-      <h3>TEAM 6</h3>
+        <div class="logo-img">
+          <img src="https://clipart.info/images/ccovers/1499955328airbnb-2-logo-png.png" alt="logo">
+        </div>
+        <div class="row boolbnb-info">
+            <div class="about-us">
+                <ul>
+                   <li><a href="#">Informazioni</a></li>
+                   <li><a href="#">Diversità</a></li>
+                   <li><a href="#">Appartenenza</a></li>
+                   <li><a href="#">Affiddabilià</a></li>
+                </ul>
+            </div>
+            <div class="about-us">
+                <ul>
+                   <li><a href="#">Informazioni</a></li>
+                   <li><a href="#">Diversità</a></li>
+                   <li><a href="#">Appartenenza</a></li>
+                   <li><a href="#">Affiddabilià</a></li>
+                </ul>
+            </div>
+            <div class="about-us">
+                <ul>
+                   <li><a href="#">Informazioni</a></li>
+                   <li><a href="#">Diversità</a></li>
+                   <li><a href="#">Appartenenza</a></li>
+                   <li><a href="#">Affiddabilià</a></li>
+                </ul>
+            </div>
+            <div class="about-us">
+                <ul>
+                   <li><a href="#">Informazioni</a></li>
+                   <li><a href="#">Diversità</a></li>
+                   <li><a href="#">Appartenenza</a></li>
+                   <li><a href="#">Affiddabilià</a></li>
+                </ul>
+            </div>
+        </div>
     </footer>
 </body>
