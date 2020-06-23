bdfd5e2cfa28b0dd47efc23f3bd58f2dc0c89d69
Michael <m.molin.1987@gmail.com>
2020-06-23 12:18:41 +0200

    Filtri funzionanti ( manca raggio km)

--- resources/js/search.js
+++ resources/js/search.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-
+    console.log('collegato');
     $('#btn-filter').on('click', function() {
         var beds = $('#beds').val();
         var rooms = $('#rooms').val();
         var bathrooms = $('#bathrooms').val();
-
+        var amenitiesfilter = amenityFilter();
         $.ajax({
             url : "http://127.0.0.1:8000/api/placesInRange",
             method :"GET",
@@ -14,20 +14,68 @@ $(document).ready(function() {
             },
             success : function (data) {
                 var risultati = data;
+                var amenitiesInPlace = [];
                 for (var i = 0; i < risultati.length; i++) {
                     var risultato = risultati[i];
-                    console.log(risultato);
-                    var context = {
-                        summary: risultato.summary,
-                        address: risultato.address,
-                        price: risultato.price,
-                        path: risultato.path
+
+
+                    if (risultato.info.beds < beds ) {
+                        $('#' + risultato.id).hide();
                         }
+                    if (risultato.info.rooms < rooms ) {
+                        $('#' + risultato.id).hide();
+                        }
+                    if (risultato.info.bathrooms < bathrooms ) {
+                        $('#' + risultato.id).hide();
+                        }
+
+
+                    $('#' + risultato.id).find('.amenities').each(function(){
+                        var amenity = parseInt($(this).data('amenities'));
+                        amenitiesInPlace.push(amenity);
+                        });
+                    console.log('i servizi sono: ' +amenitiesInPlace);
+
+                    for (var x = 0; x < amenitiesfilter.length; x++) {
+                        console.log('ameniti filter corrisponde: ' +amenitiesfilter[x]);
+                        console.log('ameniti place corrisponde: ' + amenitiesInPlace );
+                        var check = amenitiesInPlace.includes(amenitiesfilter[x]);
+                        console.log(check);
+                        if (check === false) {
+                            $('#' + risultato.id).hide();
+                        }
+                    }
+
+                    // for (var x = 0; x < amenities.length; x++) {
+                    //     console.log('amenity filtro: ' + amenities[x]);
+                    //     console.log(risultato.amenities);
+                    //     console.log('amenity in appartamento: '+ amenitiesPlace);
+                    //     var amenitiesPlace = 0;
+                    //     var check = amenitiesPlace.includes(amenities[x]);
+                    //     console.log('Questo è check: '+ check);
+                    //     if (check == false) {
+                    //         $('#' + risultato.id).hide();
+                    //     }
+                    // }
+
+                    // var check = isTrue(risultato.amenities, amenities);
                     }
                 },
             error : function () {
                 alert("E' avvenuto un errore. ");
             }
         });
+
+
+
+    function amenityFilter() { // Funzione che crea un array filters inserendo i valori delle checkbox che sono stati cliccati dall'utente
+       var filters = [];
+       $('.check-amenity').each(function(){
+           if ($(this).prop('checked') == true) {
+               filters.push(parseInt($(this).val()));
+           }
+       });
+       return filters;
+   };
     });
 });
