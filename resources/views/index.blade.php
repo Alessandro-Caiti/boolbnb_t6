6f2e1e3ff885d784e01438aeccfe6483d7d42505
Alessandro Caiti <acaiti@hotmail.it>
2020-06-25 12:38:30 +0200

    grafica5

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,24 +7,27 @@
           #mapid {height: 0px};
         </style>
 
-        <div class="index-appartamenti">
+        <div class="my-places">
             @foreach ($places as $place)
                 @if ($place->visible == 1)
-                    <a href="{{route('show', $place->id)}}">
-                        <div class="index-place-container">
-                            @foreach ($place->photo as $photo)
-                            <div class="my-place-foto">
-                                <img class="index-img" src="{{asset('storage/'  . $photo->path)}}" alt="{{$photo->name}}">
+                    <div class="my-place">
+                        <a href="{{route('show', $place->id)}}">
+                            <div class="index-place-container">
+                                @foreach ($place->photo as $photo)
+                                <div class="index-place-foto">
+                                    <img class="index-img" src="{{asset('storage/'  . $photo->path)}}" alt="{{$photo->name}}">
+                                </div>
+                                @endforeach
+                                <div class="index-place-summary">
+                                    <h2>{{$place->summary}}</h2>
+                                </div>
                             </div>
-                            @endforeach
-                            <div class="index-place-summary">
-                                <h2>{{$place->summary}}</h2>
-                            </div>
-                        </div>
-                    </a>
+                        </a>
+                    </div>
                 @endif
             @endforeach
         </div>
+
       </main>
 
 {{-- <script src="https://cdn.jsdelivr.net/npm/places.js@1.19.0"></script>
